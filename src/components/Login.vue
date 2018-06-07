808980f5e24bf5ebd0694cdacd674ba2c5af6115
liucaizhong <jojoliu12345@gmail.com>
2018-06-07 13:14:30 +0800

    compress login_bg

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -123,16 +123,13 @@ export default {
 </script>
 
 <style lang="less">
-body {
-  background: url(../assets/login_bg.jpg) fixed center center;
-}
-
 .login {
   width: 100%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: url(../assets/login_bg.jpg) fixed center center;
 
   .login-box{
     width: 440px;
